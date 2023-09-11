63f9ecd2df458fcf57bcdcc4889ac1c12fc54e7e
Mark Mburu <markmburu685@gmail.com>
2023-09-11 15:49:17 +0300

    removed comments

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,59 +1,3 @@
-// import React,{useState,useEffect} from "react";
-// import getMovieRequest from "../../services/MovieService";
-// import SearchBox from "../Search/SearchBox";
-// import MovieList from "./MovieLIst/MovieList";
-// import styles from "./Movies.module.css";
-
-// const Movies = () => {
-//     const [movies, setMovies] = useState([]);
-//     const [searchValue, setSearchValue] = useState("");
-//     const [currentPage, setCurrentPage] = useState(1);
-//     const pageSize = 8;
-
-//     useEffect(() => {
-//         const fetchData = async () => {
-//           const movieData = await getMovieRequest(searchValue);
-//           setMovies(movieData);
-//         };
-//         fetchData();
-//       }, [searchValue]);
-//   const handlePageChange = (newPage) => {
-//     setCurrentPage(newPage);
-//   };
-//   const totalPages = Math.ceil(movies.length / pageSize);
-//   return (
-//     <div>
-//       <div>
-//         <SearchBox
-//           searchValue={searchValue}
-//           setSearchValue={setSearchValue}
-//           placeholder="Type to Search A Movie"
-//         />
-//       </div>
-//       <div>
-//         {movies.length > pageSize ? (
-//           <div>
-//             <MovieList
-//               movies={movies}
-//               currentPage={currentPage}
-//               pageSize={pageSize}
-//             />
-//             <div className={styles.paginationContainer}>
-
-//               <button className={styles.previous} disabled={currentPage === 1} onClick={() => handlePageChange(currentPage - 1)}>
-//                 Previous
-//               </button>
-//               <button className={styles.next} disabled={currentPage === totalPages} onClick={() => handlePageChange(currentPage + 1)}>Next</button>
-//             </div>
-//           </div>
-//         ) : (
-//           <MovieList movies={movies} />
-//         )}
-//       </div>
-//     </div>
-//   );
-// };
-//   export default Movies;
 import React, { useState, useEffect } from "react";
 import getMovieRequest from "../../services/MovieService";
 import SearchBox from "../Search/SearchBox";
@@ -62,41 +6,42 @@ import styles from "./Movies.module.css";
 import Header from "../Header/Header";
 
 const Movies = () => {
-  const [movies, setMovies] = useState([]);
+  const [moviesData, setMoviesData] = useState({ movies: [], error: "" });
   const [searchValue, setSearchValue] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
   const [isLoading, setIsLoading] = useState(false);
-  
   const pageSize = 8;
 
   useEffect(() => {
     const fetchData = async () => {
-      if (searchValue === "") {
-        setMovies([]);
-        return;
-      }
-      setIsLoading(true);
-      const movieData = await getMovieRequest(searchValue);
+      const { movies, error } = await getMovieRequest(searchValue);
       setIsLoading(false);
-      if (movieData === 0) {
-        setMovies([]);
+
+      if (error) {
+        setMoviesData({ movies: [], error });
       } else {
-        setMovies(movieData);
-        setCurrentPage(1); // Reset the current page when new data is fetched.
+        setMoviesData({ movies, error: "" });
+        setCurrentPage(1);
       }
     };
-    fetchData();
+
+    if (searchValue !== "") {
+      setIsLoading(true);
+      fetchData();
+    } else {
+      setMoviesData({ movies: [], error: "" });
+    }
   }, [searchValue]);
 
   const handlePageChange = (newPage) => {
     setCurrentPage(newPage);
   };
 
-  const totalPages = Math.ceil(movies.length / pageSize);
+  const totalPages = Math.ceil(moviesData.movies.length / pageSize);
 
   return (
     <div>
-      <Header heading="Movie Hub"/>
+      <Header heading="Movie Hub" />
       <div>
         <SearchBox
           searchValue={searchValue}
@@ -107,41 +52,38 @@ const Movies = () => {
       <div>
         {isLoading ? (
           <div className={styles.loadingContainer}>
-            <p className={styles.loadingText}>Loading...</p> 
-            
-            </div>
-        ) : movies.length === 0 && searchValue !== "" ? (
+            <p className={styles.loadingText}>Loading...</p>
+          </div>
+        ) : moviesData.error ? (
           <div className={styles.loadingContainer}>
-            <p className={styles.loadingText}>No movies found with the title of {searchValue} .</p> 
-            
-            </div>
-    
-        ) : movies.length > pageSize ? (
+            <p className={styles.loadingText}>{moviesData.error}</p>
+          </div>
+        ) : (
           <div>
             <MovieList
-              movies={movies}
+              movies={moviesData.movies}
               currentPage={currentPage}
               pageSize={pageSize}
             />
-            <div className={styles.paginationContainer}>
-              <button
-                className={styles.previous}
-                disabled={currentPage === 1}
-                onClick={() => handlePageChange(currentPage - 1)}
-              >
-                Previous
-              </button>
-              <button
-                className={styles.next}
-                disabled={currentPage === totalPages}
-                onClick={() => handlePageChange(currentPage + 1)}
-              >
-                Next
-              </button>
-            </div>
+            {moviesData.movies.length > pageSize && (
+              <div className={styles.paginationContainer}>
+                <button
+                  className={styles.previous}
+                  disabled={currentPage === 1}
+                  onClick={() => handlePageChange(currentPage - 1)}
+                >
+                  Previous
+                </button>
+                <button
+                  className={styles.next}
+                  disabled={currentPage === totalPages}
+                  onClick={() => handlePageChange(currentPage + 1)}
+                >
+                  Next
+                </button>
+              </div>
+            )}
           </div>
-        ) : (
-          <MovieList movies={movies} />
         )}
       </div>
     </div>
--- src/services/MovieService.js
+++ src/services/MovieService.js
@@ -2,13 +2,32 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 const URI = process.env.REACT_APP_API_URL;
 
 const getMovieRequest = async (searchValue) => {
-  const uri = `${URI}?s=${searchValue}&apikey=${API_KEY}`;
-  const response = await fetch(uri);
-  const responseJson = await response.json();
-  if (responseJson.Search) {
-    return responseJson.Search;
+  try {
+    if (searchValue === "") {
+      return { movies: [], error: "" };
+    }
+
+    const uri = `${URI}?s=${searchValue}&apikey=${API_KEY}`;
+    const response = await fetch(uri);
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const responseJson = await response.json();
+
+    if (responseJson.Response === "False" && responseJson.Error === "Movie not found!") {
+      return { movies: [], error: `No movies found with the title ${searchValue} .` };
+    }
+
+    if (responseJson.Search) {
+      return { movies: responseJson.Search, error: "" };
+    }
+
+    return { movies: [], error:"" };
+  } catch (error) {
+    return { movies: [], error: "An error occurred." };
   }
-  return [];
 };
 
 export default getMovieRequest;
